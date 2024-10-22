ea6fd0d0c557b4cf3c0c59bb578b5e6596df5ae2
unknown <supereargle@gmail.com>
2024-10-22 15:58:59 +0900

    update kadai005

--- kadai_005/score.php
+++ kadai_005/score.php
@@ -1,13 +1,17 @@
 <?php 
  
-$scores = [80,60,55,40,100,25,80,95,30,60];
-$sum = 0;
+$score1 = 80;
+$score2 = 60;
+$score3 = 55;
+$score4 = 40;
+$score5 = 100;
+$score6 = 25;
+$score7 = 80;
+$score8 = 95;
+$score9 = 30;
+$score10 = 60;
 
-foreach($scores as $value){
-  global $sum;
-  $sum += $value;
-}
-
-echo "平均点は{$sum / count($scores)}です。";
-
+$sum = $score1 + $score2 + $score3 + $score4 + $score5 + $score6 + $score6 + $score7 + $score8 + $score9 + $score10;
+$ave = $sum / 10;
+echo "平均点は{$ave}です。";
 ?>
